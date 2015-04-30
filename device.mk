11b556b03e027afa88a4b1f0ab57413954844cef
Scott Mertz <scott@cyngn.com>
2015-04-29 18:34:44 -0700

    oneplus2: simple liblights
    Change-Id: Iffcbb0bccd277fad895f959b8d55626b4fa5a6cf

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
     keystore.msm8994 \
     gatekeeper.msm8994
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8994
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \

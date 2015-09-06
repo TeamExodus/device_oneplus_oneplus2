d2f58618e1a516a5451740fba33999fd1dc3b705
Raja Mungamuri <Raja1612.M@gmail.com>
2015-09-06 15:53:55 +0530

    Oneplus2 experimental build changes.
    Change-Id: Ic1123d007c3b678b9b23c4b0090d3208b2147eea

--- exodus.mk
+++ exodus.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
 
 # Inherit oneplus2-specific vendor tree
 $(call inherit-product-if-exists, vendor/oneplus/oneplus2/oneplus2-vendor.mk)

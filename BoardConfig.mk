a7821a6fc55f92096e8525b4ee83148854f680bb
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-02-12 00:02:32 +0000

    oneplus2: add TARGET_CPU_CORTEX_A53
    Change-Id: I50595a1078e9163763b1917f5c0c3d12950670da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_CPU_CORTEX_A53 := true
 TARGET_USES_64_BIT_BINDER := true
 
 # Cpusets

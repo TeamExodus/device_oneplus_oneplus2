fb0bb77e8890b4fd2d28bae7ce23adca5ac2f812
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-02-19 18:16:56 +0000

    wifi: update symlinks for config files
    * taken from plutonium
    
    Change-Id: Ia47051a8befd05793e5216a550b8c1be13e87c65

--- Android.mk
+++ Android.mk
@@ -152,12 +152,14 @@ $(shell mkdir -p $(TARGET_OUT_ETC)/firmware; \
     ln -sf /dev/block/bootdevice/by-name/msadp \
         $(TARGET_OUT_ETC)/firmware/msadp)
 
-# Create a link for the WCNSS config file, which ends up as a writable
+# Create a link for the WCNSS config files, which ends up as a writable
 # version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/qca_cld; \
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
+    ln -sf  /system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
+        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_wlan_nv.bin; \
     ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
-	    $(TARGET_OUT)/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
+        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
     ln -sf /persist/wlan_mac.bin \
-	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
+        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
 
 endif

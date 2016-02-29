a7948d2f7d354eb9f8ce08f75c75c40f4658bc0d
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-02-29 10:59:59 +0000

    oneplus2: remove dex preopt
    Change-Id: Ieaf195ea093478ca172284c8e91a6bd99004b5cf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,15 +144,6 @@ BOARD_USES_QC_TIME_SERVICES := true
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
-      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-    endif
-  endif
-endif
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true

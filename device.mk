9c587d2ab8a671c1840435d24556a7fe8079b664
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-03-07 01:55:29 +0000

    oneplus2: bring back needed blobs for location and sensors
    * also remove unneeded configs and clean props
    
    Change-Id: I1f33693d2a95bcb6fe57842f0859f7082a0e42f9

--- device.mk
+++ device.mk
@@ -147,9 +147,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # IRQ
 PRODUCT_COPY_FILES += \

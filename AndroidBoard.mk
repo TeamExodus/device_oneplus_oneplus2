918c0c2384efa686dbafb9d5f309f54a0bb6f5b7
Alex Naidis <alex.naidis@linux.com>
2016-03-20 22:51:50 +0100

    Oneplus2: ship recommended Firmware
    * there are various issues with our ROM not working fine on the different firmware releases, especially modem causes FCs
    * -> ship the firmware we recommend
    
    Change-Id: I8001613658dc75e95e089e4d4b04d5aada4691a1
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

---
+++ AndroidBoard.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+endif
+
+$(call add-radio-file,firmware/NON-HLOS.bin)
+$(call add-radio-file,firmware/static_nvbk.bin)
+$(call add-radio-file,firmware/rpm.mbn)
+$(call add-radio-file,firmware/sbl1.mbn)
+
+$(call add-radio-file,firmware/pmic.mbn)
+$(call add-radio-file,firmware/tz.mbn)
+$(call add-radio-file,firmware/hyp.mbn)
+$(call add-radio-file,firmware/BTFM.bin)

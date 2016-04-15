50cf511010effe130a43d6cad060e959310acd26
Alex Naidis <alex.naidis@linux.com>
2016-04-15 13:20:35 +0200

    device: Use Exodus BSP
    Change-Id: I5b6dc9dcc3f45ebea3d7813f6c625e5bdc8f4b7b
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.coex.sh
 
+# Exodus BSP
+TARGET_USES_EXODUS_BSP := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \

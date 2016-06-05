c7eef2e3228ccefa9d305c7a7f77ea6507d608fa
Alex Naidis <alex.naidis@linux.com>
2016-06-05 15:14:46 +0200

    oneplus2: recovery: disable Exodus Recovery for now
    * some weird build error
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- ExodusRecovery.mk
+++ ExodusRecovery.mk
@@ -3,6 +3,6 @@ TARGET_RECOVERY_FSTAB := device/oneplus/oneplus2/twrp.fstab
 TW_THEME := portrait_hdpi
 BOARD_HAS_NO_REAL_SDCARD := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-RECOVERY_VARIANT := exodus
+#RECOVERY_VARIANT := exodus
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
